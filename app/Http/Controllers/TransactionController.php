cd4656e5dc02820dfe70633482d77f813e4ed058
Reyhan Alphard Savero <newrey9227@gmail.com>
2019-10-03 15:39:41 +0700

    First Commit

---
+++ app/Http/Controllers/TransactionController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Transaction;
+use Illuminate\Http\Request;
+use Veritrans_Config;
+use Veritrans_Snap;
+use Veritrans_Notification;
+use Illuminate\Support\Str;
+use Auth;
+use Illuminate\Support\Facades\Redirect;
+
+class TransactionController extends Controller
+{
+    /**
+     * Class constructor.
+     *
+     * @param \Illuminate\Http\Request $request User Request
+     *
+     * @return void
+     */
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+
+        // Set midtrans configuration
+        Veritrans_Config::$serverKey = config('services.midtrans.serverKey');
+        Veritrans_Config::$isProduction = config('services.midtrans.isProduction');
+        Veritrans_Config::$isSanitized = config('services.midtrans.isSanitized');
+        Veritrans_Config::$is3ds = config('services.midtrans.is3ds');
+    }
+
+    public function postTransaction() {
+
+        $transaction = Transaction::create([
+            'user_id' => Auth::user()->id,
+            'name' => 'lorem ipsum',
+            'amount' => '10000',
+        ]);
+
+        // Buat transaksi ke midtrans kemudian save snap tokennya.
+        $payload = [
+            'transaction_details' => [
+                'order_id'      => $transaction->id,
+                'gross_amount'  => $transaction->amount,
+            ],
+            'customer_details' => [
+                'first_name'    => Auth::user()->id,
+                'email'         => Auth::user()->email,
+                // 'phone'         => '08888888888',
+                // 'address'       => '',
+            ],
+            'item_details' => [
+                [
+                    'id'       => rand(1, 100),
+                    'price'    => rand(1000, 10000),
+                    'quantity' => 1,
+                    'name'     => Str::random('16')
+                ]
+            ]
+        ];
+
+        $paymentUrl = Veritrans_Snap::createTransaction($payload)->redirect_url;
+        $transaction->snap_token = $paymentUrl;
+        $transaction->save();
+
+        return Redirect::to($paymentUrl);
+    }
+
+    public function requestUrl(Request $request) {
+
+    }
+}
---
+++ app/Transaction.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Transaction extends Model
+{
+    protected $fillable = [
+        'user_id', 'name', 'amount', 'status', 'snap_token'
+    ];
+
+    /**
+     * Set status to Pending
+     *
+     * @return void
+     */
+    public function setPending()
+    {
+        $this->attributes['status'] = 'pending';
+        self::save();
+    }
+
+    /**
+     * Set status to Success
+     *
+     * @return void
+     */
+    public function setSuccess()
+    {
+        $this->attributes['status'] = 'success';
+        self::save();
+    }
+
+    /**
+     * Set status to Failed
+     *
+     * @return void
+     */
+    public function setFailed()
+    {
+        $this->attributes['status'] = 'failed';
+        self::save();
+    }
+
+    /**
+     * Set status to Expired
+     *
+     * @return void
+     */
+    public function setExpired()
+    {
+        $this->attributes['status'] = 'expired';
+        self::save();
+    }
+}
